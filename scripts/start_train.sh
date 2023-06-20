81563ad0c16e99f82ecd663b49d44ecc83ea5043
jian.zhang <jian.zhang.psu@gmail.com>
2023-06-20 19:51:43 +0800

    replace start_train.sh from docker image, then know the environment is something wrong

--- scripts/start_train.sh
+++ scripts/start_train.sh
@@ -1,19 +1,16 @@
-source scripts/common.sh 
 cd RWKV-v4neo
-# cd  ~/train/RWKV-LM-LoRA/RWKV-v4neo
-
-python train.py \
---load_model "$MODEL_PATH/RWKV-4-Pile-3B-Chn-testNovel-done-ctx2048-20230312.pth" \
---proj_dir $PROJECT_PATH/lora_checkpoints \
---data_file "$DATASET_PATH/databricks-dolly-15k.jsonl" \
---data_type utf-8 \
+python3 ./train.py \
+--load_model "/root/autodl-tmp/model/RWKV-4-Pile-3B-Chn-testNovel-done-ctx2048-20230312.pth" \
+--proj_dir /root/autodl-tmp/lora_checkpoints \
+--data_file "/root/autodl-tmp/data/_text_document" \
+--data_type binidx \
 --vocab_size 50277 \
 --ctx_len 4096 \
 --accumulate_grad_batches 8 \
 --epoch_steps 100 \
---epoch_count 1 \
+--epoch_count 10 \
 --epoch_begin 0 \
---epoch_save 5 \
+--epoch_save 1 \
 --micro_bsz 1 \
 --n_layer 32 \
 --n_embd 2560 \
@@ -21,7 +18,7 @@ python train.py \
 --head_qk 0 \
 --lr_init 1e-5 \
 --lr_final 1e-5 \
---warmup_steps 0 \
+--warmup_steps 50 \
 --beta1 0.9 \
 --beta2 0.999 \
 --adam_eps 1e-8 \
@@ -32,6 +29,7 @@ python train.py \
 --grad_cp 1 \
 --lora \
 --lora_r 8 \
---lora_alpha 32 \
+--lora_alpha 64 \
 --lora_dropout 0.01 \
---lora_parts=att,ffn,time,ln
+--lora_parts=att,ffn,time,ln \
+--lora_load /root/autodl-tmp/lora_checkpoints/rwkv-12.pth \

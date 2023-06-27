08d012124d11e25fedc7463438548365e9c0d96b
Quartzing <Quartzing@users.noreply.github.com>
2023-06-27 02:50:54 +0000

    update bulid env with submodule update

--- scripts/build_env.sh
+++ scripts/build_env.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# conda create -n rwkv
-# conda activate rwkv
-# conda install python=3.10 # the highest python version that pytorch supports as of today
-# conda install pip
+
+# Fetch sub modules
+git submodule update --init
+
+# Build env for RWKV
 pip install -r requirements.txt
 
-# Build env for gpt-neox-RWKV
+# Build env for json2binidx tool
 cd json2binidx_tool
 pip install -r requirements.txt
-

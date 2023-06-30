acf40e874ca0952e5f363b5f8fa509af87bdcd39
jian.zhang <jian.zhang.psu@gmail.com>
2023-06-30 11:05:23 +0000

    add chat and chat small

--- scripts/chat-small.sh
+++ scripts/chat-small.sh
@@ -6,8 +6,8 @@ cd RWKV-v4neo
 python chat.py \
 --load_model $LOCAL_MODEL_PATH \
 --load_lora ${1} \
---n_layer 32 \
---n_embd 2560 \
+--n_layer 12 \
+--n_embd 786 \
 --ctx_len 70 \
 --lora_r 8 \
 --lora_alpha 32
